b9533845623052ef8e991e775622dd1f213055ef
rpydr125-cpu <rpydr125@gmail.com>
2026-01-19 12:58:59 +0530

    Create quality.js

---
+++ quality.js
@@ -0,0 +1,10 @@
+function detectQuality(url) {
+  if (url.includes("1080")) return "1080p";
+  if (url.includes("720")) return "720p";
+  if (url.includes("480")) return "480p";
+  if (url.includes("360")) return "360p";
+  return "auto";
+}
+
+module.exports = { detectQuality };
+
