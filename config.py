d1a2d3ac6cd2e099a36560a2a5f23d9b5fb373fb
rpydr125-cpu <rpydr125@gmail.com>
2026-01-19 12:14:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "12345")) # Apna API ID daalein
-    API_HASH = os.environ.get("API_HASH", "your_hash") # Apna API Hash daalein
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_token") # Apna Bot Token daalein
+    API_ID = int(os.environ.get("API_ID", "22447622")) # Apna API ID daalein
+    API_HASH = os.environ.get("API_HASH", "543b62d58d3e723e766ba57a984ab65d") # Apna API Hash daalein
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8296583593:AAFdO5i9cj9noPqmeVZl9kbH4nEWWwmI42w") # Apna Bot Token daalein
     PROXY_API = "https://hindibhaskarbyamarnathapi.akamai.net.in/xstream?url="
   
